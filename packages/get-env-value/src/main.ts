035a5eb1dc2515b73264da6d762505feb5cbc5c7
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-25 13:51:03 +0330

    refactor(get-env-value): separate types & refactor the function

--- packages/get-env-value/src/main.ts
+++ packages/get-env-value/src/main.ts
@@ -1,21 +1,27 @@
 import {packageTracer} from '@alwatr/package-tracer';
+import {platformInfo} from '@alwatr/platform-info';
 
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
 
-export function getEnvValue(params: { key: string; devValue: string; isRequired?: true; }): string {
-  if (__dev_mode__ === true) {
-    process.env[params.key] = params.devValue;
+interface GetEnvValueParams {
+  name: string;
+  defaultValue?: string;
+  developmentValue?: string;
+}
+
+export function getEnvValue(params: GetEnvValueParams): string {
+  if (platformInfo.development === true) {
+    const value = params.developmentValue ?? params.defaultValue;
+    if (value === undefined) {
+      throw new Error(`Set default value or development value for ${params.name} while you are developing.`);
+    }
+
+    return value;
   }
 
-  /**
-   * Is's possible we need a `env` key that required in `dev` mode too.
-   */
-  if (
-    params.isRequired === true &&
-    (process.env[params.key] === undefined || process.env[params.key] === '')
-  ) {
-    throw new Error(`${params.key} is a required ENV key.`);
+  if (process.env[params.name] === undefined || process.env[params.name] === '') {
+    throw new Error(`${params.name} is a required ENV key in production.`);
   }
 
-  return process.env[params.key] as string;
+  return process.env[params.name]!;
 }
